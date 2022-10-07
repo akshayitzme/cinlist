590ab3c7278753624bf564b44e9681c43f14d018
Akshay <akshaypooloth1@gmail.com>
2022-10-07 15:50:06 +0530

    feat: user-auth

--- cinlist/models.py
+++ cinlist/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db.models import *
+from django.contrib.auth.models import User
 
-# Create your models here.
+class List(Model):
+    pass
+
+class Genre(Model):
+    pass
+
+class Series(Model):
+    pass
+
+class Movies(Model):
+    pass
