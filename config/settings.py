cd97a1679cae07936d6c14ff172f48ed4dc5f1ae
ibradzm <56613093+ibradzm@users.noreply.github.com>
2022-10-07 11:57:55 +0100

    Debug mode off

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*_g&bnywvu$@12^*6d6qwe#oqur38w_db11=(w%+w)uz*&b^rj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
